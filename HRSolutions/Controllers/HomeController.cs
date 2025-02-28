14c31ba73e6be0ff7330c55521ddd7a825e552b7
Aakifah Asger <Aakifah Asger@DESKTOP-8FA8OTI>
2025-02-28 12:59:11 +0530

    Add project files.

---
+++ HRSolutions/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+using System.Diagnostics;
+using HRSolutions.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HRSolutions.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return PartialView(); 
+        }
+
+        public IActionResult HRanalytics()
+        {
+            return PartialView();
+        }
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
